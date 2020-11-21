c800e47b364aad0e4a737ae33389a70008cbab7e
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-11-21 13:21:03 +0100

    Small setup fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Haakon
+          Learn Haakonwefwee
         </a>
       </header>
     </div>
