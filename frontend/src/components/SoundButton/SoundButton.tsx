2ce65485f2cbcdc8f4d5aac46ca116eb59ac755f
Haakon Gunleiksrud <haakogun@stud.ntnu.no>
2020-11-30 11:34:12 +0100

    Add a minimal header and a button

--- frontend/src/components/SoundButton/SoundButton.tsx
+++ frontend/src/components/SoundButton/SoundButton.tsx
@@ -1,13 +1,14 @@
-/*
 import React from 'react';
 
 function SoundButton() {
 
-    function playSound() {
-        console.log("HEI")
-    }
+  function playSound() {
+      console.log("HEI")
+      audio.play();
+  }
 
-    const cat = require("../../assets/Cat.mp3");
+  const cat = require("../../assets/Cat.mp3");
+  let audio = new Audio("../../assets/Cat.mp3");
 
   return (
     <div>
@@ -16,46 +17,5 @@ function SoundButton() {
     </div>
   );
 }
-
-export default SoundButton;
-*/
-
-import React, { useState, useEffect } from "react";
-
-const useAudio = (url: string) => {
-  console.log("QEIFNIWE");
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => {
-        console.log("wfoewjfi");
-        setPlaying(!playing);
-    }
-
-  useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
-    [playing]
-  );
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  }, []);
-
-  return [playing, toggle];
-};
-
-const SoundButton = ( url ) => {
-  const [playing, toggle] = useAudio(url);
-
-  return (
-    <div>
-      <button onClick={() => toggle}>{playing ? "Pause" : "Play"}</button>
-    </div>
-  );
-};
 
 export default SoundButton;
